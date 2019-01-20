954ad13c5e4ad54f3a7ea73448e47f8b58f04ecd
Tim de Vos <timedevos@gmail.com>
2019-01-20 21:42:07 +0100

    werkt deze werkt

---
+++ usbcontrolwerkt/usbcontrolwerkt.ino
@@ -0,0 +1,27 @@
+#include <HID-Project.h>
+#include <HID-Settings.h>
+
+const int buttonPin = 7;          // input pin for pushbutton
+int previousButtonState = HIGH;   // for checking the state of a pushButton
+int counter = 0;                  // button push counter
+
+void setup() {
+  // make the pushButton pin an input:
+  pinMode(buttonPin, INPUT);
+  // initialize control over the keyboard:
+}
+
+void loop() {
+  // read the pushbutton:
+  int buttonState = digitalRead(buttonPin);
+  // if the button state has changed,
+  if ((buttonState != previousButtonState)
+      // and it's currently pressed:
+      && (buttonState == HIGH)) {
+    // increment the button counter
+    // type out a message 
+    Consumer.write(MEDIA_NEXT);
+  }
+
+    previousButtonState = buttonState;
+}
